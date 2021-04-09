1ae9363f8ec0049840fe2cca41cb8f0c11d195bb
Josh Nussbaum <joshnuss@gmail.com>
2021-04-08 23:18:24 -0400

    Adds template for db.js

--- preset.js
+++ preset.js
@@ -5,7 +5,7 @@ Preset.editNodePackages().addDev("@supabase/supabase-js", "^1.8.0").withTitle("I
 
 Preset.env().createIfMissing()
   .set('VITE_SUPABASE_URL', '<your-supabase-url>')
-  .set('VITE_SUPABASE_PUBLIC_KEY', '<your-supabase-public-key>')
+  .set('VITE_SUPABASE_ANON_KEY', '<your-supabase-public-key>')
   .set('SUPABASE_PRIVATE_KEY', '<your-supabase-private-key>')
   .set('SUPABASE_JWT_SECRET', '<your-supabase-jwt-secret>')
 
@@ -17,7 +17,10 @@ Preset.edit('.gitignore').update(content => {
   return content + ".env\n"
 })
 
+Preset.extract().to('src/lib')
+
 Preset.instruct([
   `Run ${color.magenta("npm install")}, ${color.magenta("pnpm install")}, or ${color.magenta("yarn")} to install dependencies`,
-  `Add your supabase keys to your ${color.green('.env')} file`
+  `Add your supabase keys to your ${color.green('.env')} file`,
+  `You can access the db via ${color.green("import supabase from '$lib/db'")}`
 ]).withHeading('Completing setup')
---
+++ templates/db.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+)
+
+export default suspabase
