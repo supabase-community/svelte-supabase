621fd78082556a9b1030c11f35eb1b20dd3fb93d
William Wall <william.a.wall.4@gmail.com>
2022-04-14 03:48:49 -0600

    re-add instructional comment

--- templates/auth/routes/auth/example.js
+++ templates/auth/routes/auth/example.js
@@ -2,6 +2,10 @@ import supabase from '$lib/db';
 
 /** @type {import('./example').RequestHandler} */
 export async function get() {
+	// ***************************************************
+	// Edit the table name to a table with data in your db
+	// (preferably one with RLS and authenticated can select)
+	// ***************************************************
 	const { data, error } = await supabase.from('your-table-here').select();
 
 	if (error) {
